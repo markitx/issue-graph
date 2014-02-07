2cc3affa00bdbc0c5624e487e4459908b302d0d6
hlewis <hlewis@DeepThought.(none)>
2014-02-07 14:40:16 -0600

    reads oauth code from file. filepath in argument.

--- githubtest.js
+++ githubtest.js
@@ -1,4 +1,19 @@
 var GitHubApi = require('github');
+//var process = require('process');
+var fs = require('fs');
+
+
+if (process.argv[2] != null) { //argument is a path to file containing oauth
+	fs.readFile(process.argv[2], 'utf8', function (err, data) {
+		var oauth = data;
+		console.log(oauth);
+	});
+} else {
+	console.error( new Error('need argument'));
+	process.exit(1);
+}
+
+
 
 var github = new GitHubApi({
 	version: '3.0.0',
@@ -15,6 +30,7 @@ github.authenticate({
 github.issues.getAll({}, function(err, res) {
 	var arr = {}
 	for (issue in res){
+		console.log
 		arr[res.id] = res.body;
 	}
 	for (id in arr){
