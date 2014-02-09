9014ff34db54eb748424c859415bd550fe34ad46
hlewis <hlewis@DeepThought.(none)>
2014-02-09 10:12:47 -0600

    Completed passing in oauth.  Assembling node and edge objects.

--- githubtest.js
+++ githubtest.js
@@ -1,5 +1,4 @@
 var GitHubApi = require('github');
-//var process = require('process');
 var fs = require('fs');
 
 var oauth = "";
@@ -8,42 +7,62 @@ var oauth = "";
 if (process.argv[2] != null) { //argument is a path to file containing oauth
 	fs.readFile(process.argv[2], 'utf8', function (err, data) {
 		var oauth = data;
+
+		var github = new GitHubApi({
+			version: '3.0.0',
+		});
+
+
+
+		github.authenticate({
+			type: "oauth",
+			token: oauth
+		});
+
+		github.issues.repoIssues({
+			user: "hjylewis",
+			repo: "issue-graph"
+			//state: "open"
+		}, function(err, res) {
+			var arr = {}
+			res.forEach( function(value) {
+				arr[value.number] = value.body;
+			});
+			for (number in arr){
+				console.log(number +" "+ arr[number]);
+			}
+		})
+
+
 	});
 } else {
 	console.error( new Error('need argument'));
 	process.exit(1);
 }
 
+function node(number, edges) {
+	this.number = number; //issue number
+	this.edges = edges; //array of paths
+}
 
 
-var github = new GitHubApi({
-	version: '3.0.0',
-});
+function edge(dest, type) {
+	this.dest = dest; //mentioned issue
+	this.ptype = type; //type of mention
+}
+
+ 
 
 
-
-github.authenticate({
-	type: "oauth",
-	token: "ed0752e3d5558479009f5442b2341edc49c140ce"
-});
 
 
 // github.issues.getAll({
 // 	//state: "open"
 // }, function(err, res) { //arr maps id # to body
-// 	var arr = {}
-// 	res.forEach( function(value) {
-// 		arr[value.id] = value.body;
-// 	});
-// 	for (id in arr){
-// 		console.log(id +" "+ arr[id]);
-// 	}
+
 // });
 
-github.issues.getRepoIssue({
-	user: "hjylewis"
-	repo: "issue-graph"
-})
+
 
 // github.events.get({
 // 	org: "markitx"
