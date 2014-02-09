a67961758bea7941f89f77011e567d579438ac0c
hlewis <hlewis@DeepThought.(none)>
2014-02-09 11:07:43 -0600

    Cleaning up

--- githubtest.js
+++ githubtest.js
@@ -43,13 +43,14 @@ if (process.argv[2] != null) { //argument is a path to file containing oauth
 }
 
 function package(arr) {//param: hashtable of number to msg body
-	arr_nodes = [];
+	var arr_nodes = [];
 	for(number in arr){
 		arr_nodes.push(new node(number, getEdges(arr[number])));
 	}
+	return arr_nodes;
 }
 
-function getEdges(body){
+function getEdges(body){//takes issue body text, parses it and returns array of edges
 	var lines = body.toLowerCase().split('\n');
 	var edges = [];
 	for(i in lines){
@@ -72,34 +73,3 @@ function edge(dest, type) {
 	this.dest = dest; //mentioned issue
 	this.ptype = type; //type of mention
 }
-
- 
-
-
-
-
-// github.issues.getAll({
-// 	//state: "open"
-// }, function(err, res) { //arr maps id # to body
-
-// });
-
-
-
-// github.events.get({
-// 	org: "markitx"
-// }, function(err, res){
-// 	console.log(JSON.stringify(res,null,2));
-// });
-
-
-
-// github.user.getFollowingFromUser({
-//     // optional:
-//     // headers: {
-//     //     "cookie": "blahblah"
-//     // },
-//     user: "mikedeboer"
-// }, function(err, res) {
-//     console.log(JSON.stringify(res,null,2));
-// });
