4d6cc81be0278ca4536058e2cba120b7b48749df
hlewis <hlewis@DeepThought.(none)>
2014-03-07 14:31:39 -0600

    Now retrieves every page of issues.

--- githubtest.js
+++ githubtest.js
@@ -2,6 +2,12 @@ var GitHubApi = require('github');
 var fs = require('fs');
 
 var oauth = "";
+var keywords = [];
+var nodes = [];
+var links = [];
+var repos = [];
+var nodesByNumber = {};
+var count = 0;
 
 
 	//argument is a path to file containing oauth
@@ -27,93 +33,106 @@ fs.readFile("oauth", 'utf8', function (err, data) {
 		if (err) {
 			return console.log(err);
 		}
-		var keywords = (JSON.parse(data).keywords);
+		keywords = (JSON.parse(data).keywords);
 		keywords.push("n/a");
-		var nodes = [];
-		var links = [];
-		var nodesByNumber = {};
+
 
 		github.repos.getFromOrg({
 			org: "markitx"
 		}, function(err, res) {
-			var count = 0;
-			var repos = [];
 			var repos_length = res.length;
 			res.forEach( function(repo,index) { //repo is each repo in org
 				repos.push(repo.name);
-				github.issues.repoIssues({
-					user: "markitx",
-					repo: repo.name
-					//state: "open"
-				}, function(err, res) {
-					if (err) {
-						console.log('Error!');
-						return console.log(err);
-					}
-					res.forEach( function(value) {
-						// copy over just the values we want
-						var node = {
-							id: '' + value.id,
-							number: value.number,
-							title: value.title,
-							body: value.body,
-							repo: repo.name
-						};
-						nodes.push(node);
-						nodesByNumber["markitx/" +node.repo + '#' + node.number] = node;
-					});
+				getIssues(github,repo.name,1, repos_length);
 
-					var linkPattern = /[a-z]* ?[a-z-_/]*#\d+/gi;
-					var keywordRegEx = new RegExp(keywords.join('|')); //regex for keywords
-
-					nodes.forEach(function (node) {
-						var matches = node.body.match(linkPattern);
-						if (matches && matches.length > 0) {
-							matches.forEach(function (match) {
-								var type = match.match(keywordRegEx);
-								if(type == null) type = "n/a";
-								var id = match.match(/[a-z-_/]+#\d+/i);
-								if (id && id.length >0) {
-									var linkedTo = nodesByNumber[id];
-								} else {
-									var id = match.match(/#\d+/);
-
-									if (id && id.length >0) {
-										var linkedTo = nodesByNumber["markitx/" + node.repo + id[0].replace(/ /, "")];
-									}
-								}
-								if (linkedTo) {
-									links.push({
-										source: node,
-										target: linkedTo,
-										type: type
-									});
-								}
-
-							});
-						}
-					});
-					count++;
-					if(count == repos_length){
-						var graphData = {
-							keywords: keywords,
-							repos: repos,
-							nodes: nodes,
-							links: links
-						};
-						console.log(JSON.stringify(graphData, null, 2));
-
-						fs.writeFile('graph-data.json', 'graphData = ' + JSON.stringify(graphData), function (err) {
-							if (err) {
-								return console.log(err);
-							}
-						console.log('graph-data.json udpated');
-						});
-					}
-
-				});
 			});
 
 		});
 	});
 });
+
+function getIssues(github,name,pageN,repos_length){
+	console.log(count);
+	github.issues.repoIssues({
+		user: "markitx",
+		repo: name,
+		page: pageN,
+		per_page: 100
+		//state: "open"
+	}, function(err, res) {
+		//console.log(res.length);
+		if (err) {
+			console.log('Error!');
+			return console.log(err);
+		}
+		//console.log(res.length);
+		res.forEach( function(value) {
+			// copy over just the values we want
+			var node = {
+				id: '' + value.id,
+				number: value.number,
+				title: value.title,
+				body: value.body,
+				repo: name
+			};
+			nodes.push(node);
+			nodesByNumber["markitx/" +node.repo + '#' + node.number] = node;
+		});
+		if (res.length == 0){
+			count++;
+			if(count == repos_length){
+				writeData();
+			}
+		} else {
+			getIssues(github, name, pageN+1, repos_length);
+		}
+
+
+	});
+}
+
+function writeData(){
+	var linkPattern = /[a-z]* ?[a-z-_/]*#\d+/gi;
+	var keywordRegEx = new RegExp(keywords.join('|')); //regex for keywords
+	nodes.forEach(function (node) {
+		var matches = node.body.match(linkPattern);
+		if (matches && matches.length > 0) {
+			matches.forEach(function (match) {
+				var type = match.match(keywordRegEx);
+				if(type == null) type = "n/a";
+				var id = match.match(/[a-z-_/]+#\d+/i);
+				if (id && id.length >0) {
+					var linkedTo = nodesByNumber[id];
+				} else {
+					var id = match.match(/#\d+/);
+
+					if (id && id.length >0) {
+						var linkedTo = nodesByNumber["markitx/" + node.repo + id[0].replace(/ /, "")];
+					}
+				}
+				if (linkedTo) {
+					links.push({
+						source: node,
+						target: linkedTo,
+						type: type
+					});
+				}
+
+			});
+		}
+	});
+	var graphData = {
+		keywords: keywords,
+		repos: repos,
+		nodes: nodes,
+		links: links
+	};
+	console.log(JSON.stringify(graphData, null, 2));
+
+	fs.writeFile('graph-data.json', 'graphData = ' + JSON.stringify(graphData), function (err) {
+		if (err) {
+			return console.log(err);
+		}
+	console.log('graph-data.json udpated');
+	});
+}
